22ea74fd5fea529997d44956a139348b7a3ca975
chris.grd.alt@gmail.com <chris.grd.alt@gmail.com>
2025-08-18 02:58:09 +0000

    Created QuizAdmin; Edited Base; Deleted QuizAdmin2

--- client_code/QuizAdmin/__init__.py
+++ client_code/QuizAdmin/__init__.py
@@ -1,4 +1,4 @@
-from ._anvil_designer import QuizAdmin2Template
+from ._anvil_designer import QuizAdminTemplate
 from anvil import *
 import anvil.server
 import anvil.users
@@ -7,9 +7,14 @@ import anvil.tables.query as q
 from anvil.tables import app_tables
 
 
-class QuizAdmin2(QuizAdmin2Template):
+class QuizAdmin(QuizAdminTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
 
+    self.user = anvil.users.get_user()
+    if not self.user:
+      anvil.users.login_with_form()
+
+    open_form("QuizCatalogue")
     # Any code you write here will run before the form opens.
